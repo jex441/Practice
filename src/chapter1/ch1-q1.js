29d588eb1ede9253d1eec564d5c1f36d34bd0288
Jeffrey Wood <jwfirenze@gmail.com>
2022-01-19 10:34:29 -0500

    added comments

--- src/chapter1/ch1-q1.js
+++ src/chapter1/ch1-q1.js
@@ -8,9 +8,14 @@
  * @return {boolean}      True if unique characters, otherwise false
  */
 export function hasUniqueCharactersSet(str) {
+  // declare an empty object to store keys and values
   let obj = {};
+
+  // default result is true
   let result = true;
 
+  // loop through string, if the character is in the object, increment it's value (count) by one
+  // if the character is not in the object, set its value to 1
   for (let i = 0; i < str.length; i++) {
     if (obj[str[i]]) {
       obj[str[i]] += 1;
@@ -19,12 +24,14 @@ export function hasUniqueCharactersSet(str) {
     }
   }
 
+  // loop through object's values. If a value is greater than 1, set result to false and exit the loop
   for (let key in obj) {
     if (obj[key] > 1) {
       result = false;
       break;
     }
   }
-  console.log("OBJ", obj);
+
+  // return boolean result
   return result;
 }
