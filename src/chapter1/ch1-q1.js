5aa3f0815e5043227d6a8f7e96c0d321ff422521
Jeffrey Wood <jwfirenze@gmail.com>
2022-01-19 10:22:12 -0500

    ch1-q1 complete

--- src/chapter1/ch1-q1.js
+++ src/chapter1/ch1-q1.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /**
  * Keep track of seen characters with a Set data structure, fail when
@@ -11,5 +11,23 @@
  * @return {boolean}      True if unique characters, otherwise false
  */
 export function hasUniqueCharactersSet(str) {
-  
+  let obj = {};
+  let result = true;
+
+  for (let i = 0; i < str.length; i++) {
+    if (obj[str[i]]) {
+      obj[str[i]] += 1;
+    } else {
+      obj[str[i]] = 1;
+    }
+  }
+
+  for (let key in obj) {
+    if (obj[key] > 1) {
+      result = false;
+      break;
+    }
+  }
+  console.log("OBJ", obj);
+  return result;
 }
