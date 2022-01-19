1e4b5fc8b3c2fa6820aa481e16fe4ef8f6de2eb4
Jeffrey Wood <jwfirenze@gmail.com>
2022-01-19 10:31:07 -0500

    cleared comment

--- src/chapter1/ch1-q1.js
+++ src/chapter1/ch1-q1.js
@@ -3,10 +3,7 @@
 /**
  * Keep track of seen characters with a Set data structure, fail when
  * a repeated character is found.
- *
- * Time: O(N)
- * Additional space: O(N)
- *
+ 
  * @param  {string[]} str String to check, passed in as a character array
  * @return {boolean}      True if unique characters, otherwise false
  */
